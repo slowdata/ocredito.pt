302282c5df8a5d1e9f91d11aeee48cd4ef5567e3
slowdata <slowmotion@outlook.com>
2018-11-04 22:55:34 +0000

    Change contact props and add favico

--- src/components/container.js
+++ src/components/container.js
@@ -7,24 +7,21 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelopeSquare } from "@fortawesome/free-solid-svg-icons";
 import { faMobileAlt } from "@fortawesome/free-solid-svg-icons";
 
-export default props => (
+export default ({ contact }) => (
   <section className="hero is-fullheight is-danger">
     <div className="hero-body">
       <div className="container">
-        <h1 className="title">{props.title}</h1>
+        <h1 className="title">{contact.title}</h1>
         <span />
-        <h2 className="subtitle">{props.subtitle}</h2>
+        <h2 className="subtitle">{contact.subtitle}</h2>
         <span />
         <div className={containerStyles.contact}>
           <FontAwesomeIcon icon={faEnvelopeSquare} size="2x" />
-          <a
-            href="mailto:ocredito.pt@gmail.com"
-            className={containerStyles.foo}
-          >
+          <a href={contact.email} className={containerStyles.foo}>
             ocredito.pt@gmail.com
           </a>
           <FontAwesomeIcon icon={faMobileAlt} size="2x" />
-          <a href="tel:937384062" className={containerStyles.foo}>
+          <a href={contact.phone} className={containerStyles.foo}>
             937384062
           </a>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,12 @@ import React from "react";
 
 import Container from "../components/container";
 
-export default () => (
-  <Container
-    title="OCredito.pt"
-    subtitle="(Brevemente...) O seu novo canto para informação, dúvidas e ajuda. "
-  />
-);
+const contact = {
+  title: "OCredito.pt",
+  subtitle:
+    "(Brevemente...) O seu novo canto para informação, dúvidas e ajuda.",
+  email: "ocredito.pt@gmail.com",
+  phone: "+351-937 384 062"
+};
+
+export default () => <Container contact={contact} />;
