2f8c7a7b1ea8b1fd0a46060b6682e0f5c3781246
Ricardo Dias <slowmotion@outlook.com>
2019-11-28 17:04:22 +0000

    Start of Nav component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { Link } from "gatsby";
 
+import Nav from "../components/nav";
+
 //import Container from "../components/container";
 
 // const contact = {
@@ -27,17 +29,22 @@ class indexPage extends Component {
           style={{ transition: "all 0.5s ease-out" }}
         >
           <div className="py-2 px-4 flex justify-between">
-            <Link className="ml-2 flex items-center focus:outline-none" to="/">
-              <span
-                className="w-10 h-10 flex items-center justify-center 
-              rounded-full text-white font-semibold text-xl bg-pink-600"
+            <div>
+              <Link
+                className="ml-2 flex items-center focus:outline-none"
+                to="/"
               >
-                €
-              </span>
-              <span className="pl-1 text-xl text-pink-800 ">credito.pt</span>
-            </Link>
+                <span
+                  className="w-10 h-10 flex items-center justify-center 
+                rounded-full text-white font-semibold text-xl bg-pink-600"
+                >
+                  €
+                </span>
+                <span className="pl-1 text-xl text-pink-800 ">credito.pt</span>
+              </Link>
+            </div>
             <button
-              className="mr-4 rounded hover:bg-pink-200 focus:outline-none"
+              className="sm:md:lg:xl:hidden mr-4 rounded hover:bg-pink-200 focus:outline-none"
               onClick={() => this.handleClick()}
             >
               <div
@@ -65,7 +72,6 @@ class indexPage extends Component {
                     ? "hidden"
                     : "w-10 h-10 px-2 flex justify-center items-center"
                 } `}
-                styles={{ transition: "all 0.5s ease-out" }}
               >
                 <svg className="w-12 h-12" viewBox="0 0 40 40" fill="none">
                   <path
@@ -80,42 +86,8 @@ class indexPage extends Component {
               </div>
             </button>
           </div>
-          {/* <nav
-            className={`${leftMenu ? "" : "hidden"} 
-              flex flex-col items-center
-            `}
-          >
-            <ul className="pt-10 text-pink-800 font-semibold text-center">
-              <li>
-                <Link to="/about">About</Link>
-              </li>
-              <li className="pt-2">
-                <Link>Artigos</Link>
-              </li>
-              <li className="pt-2">
-                <Link>Contactos</Link>
-              </li>
-            </ul>
-          </nav> */}
         </header>
-        <nav
-          className={`${
-            !leftMenu ? "opacity-0" : "opacity-1"
-          } min-h-screen bg-pink-200`}
-          style={{ transition: "all 0.5s ease-out" }}
-        >
-          <ul className="pt-10 text-pink-800 font-semibold text-center">
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li className="pt-2">
-              <Link>Artigos</Link>
-            </li>
-            <li className="pt-2">
-              <Link>Contactos</Link>
-            </li>
-          </ul>
-        </nav>
+        <Nav showMenu={leftMenu} className="bg-grey-900" />
       </>
     );
   }
