7d610cc66cc9653a10bf4c7debc0428d5880be7d
Ricardo Dias <slowmotion@outlook.com>
2020-02-24 15:19:22 +0000

    Fix bug in header component import

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-import Header from "../components/Header";
+import Header from "../components/header";
 
 //import Container from "../components/container";
 
