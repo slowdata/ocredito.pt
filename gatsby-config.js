d278d5c8276b266520b852b1f76147e13a2f3439
slowdata <slowmotion@outlook.com>
2020-03-02 00:30:37 +0000

    Configurations for creditos and parcerias

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,16 @@
 module.exports = {
   siteMetadata: {
-    title: "O Credito"
+    title: "O Credito",
+    creditos: [
+      { id: 1, name: "Habitação" },
+      { id: 2, name: "Pessoal" },
+      { id: 3, name: "Automóvel" },
+      { id: 4, name: "Transferência de créditos" }
+    ],
+    parcerias: [
+      { id: 1, name: "Imobiliárias" },
+      { id: 2, name: "Seguradoras" }
+    ]
   },
   plugins: [
     {
